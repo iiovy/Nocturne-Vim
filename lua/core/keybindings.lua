07c3f1b2cc71e6d6179550effec89c72fa61bc73
Alice <182596298+iiovy@users.noreply.github.com>
2026-06-05 17:55:56 +0100

    Update neovim config

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -1,7 +1,50 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
+local map = vim.keymap.set
 
-vim.keymap.set("n", "<leader>s", "<cmd>Oil --float<CR>", { desc = "Open Oil (float)" })
+-- Files
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
+map("n", "<leader>Q", "<cmd>qa!<CR>", { desc = "Quit all" })
 
+-- Oil
+map("n", "<leader>e", "<cmd>Oil --float<CR>", { desc = "File explorer" })
+
+-- Buffers
+map("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
+map("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+map("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+
+-- Window navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- Window splits
+map("n", "<leader>sv", "<C-w>v", { desc = "Vertical split" })
+map("n", "<leader>sh", "<C-w>s", { desc = "Horizontal split" })
+map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close split" })
+
+-- ToggleTerm
+map("n", "<leader>t", "<cmd>ToggleTerm<CR>", { desc = "Terminal" })
+
+-- Search highlight
+map("n", "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear search" })
+
+-- Gitsigns
+map("n", "]h", "<cmd>Gitsigns next_hunk<CR>")
+map("n", "[h", "<cmd>Gitsigns prev_hunk<CR>")
+
+map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>")
+map("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>")
+map("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>")
+
+-- LSP 
+map("n", "gd", vim.lsp.buf.definition)
+map("n", "gD", vim.lsp.buf.declaration)
+map("n", "gr", vim.lsp.buf.references)
+map("n", "K", vim.lsp.buf.hover)
+
+map("n", "<leader>rn", vim.lsp.buf.rename)
+map("n", "<leader>ca", vim.lsp.buf.code_action)
